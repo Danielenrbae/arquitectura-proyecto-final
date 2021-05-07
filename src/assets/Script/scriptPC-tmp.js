6af61b575cae421402c1e9b3fbaba6fbe11037f6
ATSISTEMAS\denriquez <denriquez@atsistemas.com>
2021-05-07 19:09:48 +0200

    [ADD] add component footer

--- src/assets/Script/scriptPC-tmp.js
+++ src/assets/Script/scriptPC-tmp.js
@@ -2,13 +2,7 @@
 
 
 
-$(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussiness__desplegable").toggleClass("show");
-  });
-  $(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussines__desplegable").toggleClass("show");
-  });
-  
+
 $(".pf-container__desplegable").on("click", function () {
   $(".pf-nav__desplegable").toggleClass("show");
 });
@@ -17,3 +11,11 @@ $(".pf-auth__icon").on("click", function () {
   $(".pf-auth__options").toggle();
 });
 
+
+$(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussiness__desplegable").toggleClass("show");
+  });
+  $(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussines__desplegable").toggleClass("show");
+  });
+  
--- src/assets/Script/scriptPC.js
+++ src/assets/Script/scriptPC.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
 
 
+
 $(".pf-container__desplegable").on("click", function () {
   $(".pf-nav__desplegable").toggleClass("show");
 });
