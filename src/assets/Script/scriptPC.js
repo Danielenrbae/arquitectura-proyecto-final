5536ae271bd622449bb6076063161bfa7b5228c3
danielenrbae <danielenrbae@gmail.com>
2021-05-09 00:33:24 +0200

    [ADD] add profile1

--- src/assets/Script/scriptPC.js
+++ src/assets/Script/scriptPC.js
@@ -11,20 +11,13 @@ $(document).ready(function(){
 
 
 
+
 $(".pf-modal-sign-in__close").on("click", function () {
     $(".pf-modal-sign-in").css("display", "none");
 });
-
 $(".pf-modal-sign-up__close").on("click", function () {
     $(".pf-modal-sign-up").css("display", "none");
 });
-$(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussiness__desplegable").toggleClass("show");
-  });
-  $(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussines__desplegable").toggleClass("show");
-  });
-  
 $(".pf-container__desplegable").on("click", function () {
   $(".pf-nav__desplegable").toggleClass("show");
 });
@@ -33,6 +26,14 @@ $(".pf-auth__icon").on("click", function () {
   $(".pf-auth__options").toggle();
 });
 
+
+$(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussiness__desplegable").toggleClass("show");
+  });
+  $(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussines__desplegable").toggleClass("show");
+  });
+  
 
 
 });
