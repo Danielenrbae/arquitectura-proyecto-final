c50f4c568736735a8cdd5300b873bcec454f8a4d
danielenrbae <danielenrbae@gmail.com>
2021-05-09 10:48:55 +0200

    [ADD] add profile2

--- src/assets/Script/scriptPC-tmp.js
+++ src/assets/Script/scriptPC-tmp.js
@@ -12,9 +12,17 @@
 $(".pf-modal-sign-in__close").on("click", function () {
     $(".pf-modal-sign-in").css("display", "none");
 });
+$(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussiness__desplegable").toggleClass("show");
+  });
+  $(".pf-container__desplegable").on("click", function () {
+    $(".pf-nav-bussines__desplegable").toggleClass("show");
+  });
+  
 $(".pf-modal-sign-up__close").on("click", function () {
     $(".pf-modal-sign-up").css("display", "none");
 });
+
 $(".pf-container__desplegable").on("click", function () {
   $(".pf-nav__desplegable").toggleClass("show");
 });
@@ -24,10 +32,3 @@ $(".pf-auth__icon").on("click", function () {
 });
 
 
-$(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussiness__desplegable").toggleClass("show");
-  });
-  $(".pf-container__desplegable").on("click", function () {
-    $(".pf-nav-bussines__desplegable").toggleClass("show");
-  });
-  
--- src/assets/Script/scriptPC.js
+++ src/assets/Script/scriptPC.js
@@ -36,6 +36,7 @@ $(".pf-container__desplegable").on("click", function () {
   
 
 
+
 });
 
 
