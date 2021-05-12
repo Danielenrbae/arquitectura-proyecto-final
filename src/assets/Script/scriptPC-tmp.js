b0b0c5881cc7b508b04295619c64ff72afd73441
ATSISTEMAS\denriquez <denriquez@atsistemas.com>
2021-05-12 17:03:52 +0200

    [ADD] add component carousel

--- src/assets/Script/scriptPC-tmp.js
+++ src/assets/Script/scriptPC-tmp.js
@@ -1,4 +1,26 @@
 
+$(".pf-carousel__owl").owlCarousel({
+  loop: true,
+  margin: 50,
+  nav: true,
+  dots: false,
+  autoWidth: false,
+  items: 4,
+  mouseDrag: true,
+  responsive: {
+    0: {
+      items: 1,
+      dots: true,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
 
 
 
